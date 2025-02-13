be47641d4fc9536a1e9a91a73c993ed7fba8eceb
navid <nav.khosravi@gmail.com>
2025-02-13 08:37:33 +0100

    fix: fixa en till liten bug

--- application/app.py
+++ application/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from application import func
+import func
 from datetime import datetime, timedelta                    # Används för att hämta dagens datum
 
 
